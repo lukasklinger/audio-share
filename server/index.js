077aa9eede509609b66b3b3fe9906b4aaeefa1d6
Lukas <lukas@lukasklinger.com>
2022-08-02 21:38:30 +0200

    init code

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const http = require('http')
+const server = http.createServer(app)
+
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html')
+})
+
+server.listen(3000, () => {
+    console.log('listening on *:3000')
+})
