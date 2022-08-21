7c316ed324ae6274f0404bee9c04fa8df15e52f4
Lukas <lukas@lukasklinger.com>
2022-08-21 16:47:14 +0200

    add queue buffer

--- server/public/assets/js/index.js
+++ server/public/assets/js/index.js
@@ -2,20 +2,6 @@ var socket = null
 var streaming = false
 var audioBuffer = new Queue()
 
-var mediaSource = null
-var sourceBuffer = null
-var audio = document.createElement('audio')
-
-function init() {
-    mediaSource = new MediaSource();
-    
-    mediaSource.addEventListener('sourceopen', () => {
-        sourceBuffer = mediaSource.addSourceBuffer('audio/webm')
-    })
-
-    audio.src = URL.createObjectURL(mediaSource)
-}
-
 function clickHandler() {
     if (!streaming) {
         // start
@@ -59,6 +45,8 @@ function connect() {
     var roomID = getRoomID()
     socket = io()
 
+    console.log("Joining room: " + roomID)
+
     // join room to start receiving data
     socket.emit("roomID", roomID)
 
@@ -69,14 +57,18 @@ function connect() {
 
     // pipe audio chunks into decoder
     socket.on("audio", function (b64DataURL) {
-        sourceBuffer.appendBuffer(dataURItoArrayBuffer(b64DataURL))
+        audioBuffer.enqueue(b64DataURL)
+        console.log("data")
     });
 
     setTimeout(startPlayback, 2000)
 }
 
 function startPlayback() {
-    audio.play()
+    setInterval(() => {
+        var audio = new Audio(audioBuffer.dequeue())
+        audio.play()
+    }, 500)
 }
 
 function disconnect() {
@@ -88,26 +80,4 @@ function disconnect() {
 function getRoomID() {
     var pathArray = window.location.pathname.split('/')
     return pathArray[1]
-}
-
-function dataURItoArrayBuffer(dataURI) {
-    // convert base64 to raw binary data held in a string
-    // doesn't handle URLEncoded DataURIs - see SO answer #6850276 for code that does this
-    var byteString = atob(dataURI.split(',')[1]);
-
-    // separate out the mime component
-    var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0]
-
-    // write the bytes of the string to an ArrayBuffer
-    var ab = new ArrayBuffer(byteString.length);
-
-    // create a view into the buffer
-    var ia = new Uint8Array(ab);
-
-    // set the bytes of the buffer to the correct values
-    for (var i = 0; i < byteString.length; i++) {
-        ia[i] = byteString.charCodeAt(i);
-    }
-    
-    return ab;
 }
